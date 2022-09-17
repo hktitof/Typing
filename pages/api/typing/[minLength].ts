497986ec74edcbbfd9195d6a3b682f28161133b5
titof <hktitof@gmail.com>
2022-09-17 21:58:58 +0100

    fixed 'API resolved without sending a response' message

--- pages/api/typing/[minLength].ts
+++ pages/api/typing/[minLength].ts
@@ -1,15 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const getQuote = async () => {
-    await fetch("https://api.quotable.io/random?minLength=" + req.query.minLength.toString())
-      .then(response => response.json())
-      .then(data => {
-        res.status(200).json({ quote: data.content, author: data.authorSlug });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err });
-      });
-  };
-  getQuote();
+export default async  function handler(req: NextApiRequest, res: NextApiResponse) {
+  await fetch("https://api.quotable.io/random?minLength=" + req.query.minLength.toString())
+    .then(response => response.json())
+    .then(data => {
+      res.status(200).json({ quote: data.content, author: data.authorSlug });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
 }
+
+// export const config = {
+//   api: {
+//     externalResolver: true,
+//   },
+// }
